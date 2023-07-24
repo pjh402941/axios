b7130840ce84cdddd2ea98c100a54f173ca525d7
pjh <pjh402941@gmail.com>
2023-07-25 06:27:04 +0900

    initial commit

---
+++ src/components/NewsList.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import axios from "axios";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: "제목",
+  description: "내용내용",
+  url: "https://www.naver.com/",
+  urlToImage:
+    "https://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg",
+};
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === "all" ? "" : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=86e08ead809e4459bbd74e0715d51aa4`
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article}></NewsItem>
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
